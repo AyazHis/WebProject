61accd89204ddcb4786798f8f83932d7317bd3d3
ayaz.khismatullin@gmail.com <ayaz.khismatullin@gmail.com>
2022-06-01 17:58:52 +0300

    dd

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebProject.Areas.Identity.Data;
+
+namespace WebProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserDBContext _db;
+        public AdminController (UserDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<WPUser> users = _db.Users;
+            return View(users);
+        }
+        public IActionResult Edit()
+        {
+            IEnumerable<WPUser> users = _db.Users;
+            return View(users);
+        }
+
+
+    }
+}
